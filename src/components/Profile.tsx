bd90bff20de003af173d4a16a8dec0ce9af5a5a0
maks1394 <gusevmax9@yandex.com>
2022-11-13 13:46:03 +0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const Profile = ()=>{
+    return(
+        <div className={'content'}>
+            <div>
+                <img
+                    src={'https://consolidatedoffice.ca/wp-content/themes/options/images/skins/headers/full_width/header-purpleHaze.jpg'}/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+            </div>
+            <div>
+                posts
+                <div>post1</div>
+                <div>post2</div>
+            </div>
+        </div>
+    );
+}
