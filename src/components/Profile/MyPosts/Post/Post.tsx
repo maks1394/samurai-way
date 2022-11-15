6841f70a337e981da5513aeb6e5747927cf74aca
maks1394 <gusevmax9@yandex.com>
2022-11-15 19:47:56 +0300

    Improvement of Post module css

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import s from './Post.module.css'
 
 export const Post = () => {
     return (
-        <div>post1</div>
+        <div className={s.card}>
+            <div className={s.card_ava_name}>
+                <img className={s.rounded_avatar}
+                     src={'https://pbs.twimg.com/media/FckWa2RXoAAeDT1?format=jpg&name=large'}/>
+                <h4>
+                    Name <br/>
+                    <span>22:00</span>
+                </h4>
+            </div>
+            <div className={s.card_body}>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor,
+                    ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra.
+                    Proin blandit ac massa sed rhoncus
+                </p>
+            </div>
+            <div className={s.card_body}>
+                <button>Like</button>
+                10 Likes
+            </div>
+        </div>
     );
 }
