d22ad45aec553bf15f0e914e17af651d513fe0e1
maks1394 <gusevmax9@yandex.com>
2022-11-24 14:26:25 +0300

    UI-BLL

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,48 +3,66 @@ import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
 type DialogsItemPropsType = {
-    name:string;
-    id:number;
-}
-const DialogItem =(props:DialogsItemPropsType)=>{
-    return(
-        <li><NavLink to={`/dialogs/${props.id}`} className={s.item}>{props.name}</NavLink></li>
-    );
+    name: string;
+    id: number;
 }
 
 type MessagePropsType = {
-    text:string;
+    text: string;
 }
-const Message = (props:MessagePropsType)=>{
-    return(
+
+
+export function Dialogs() {
+    let dialogsData = [
+        {id: 1, name: 'Dima'},
+        {id: 2, name: 'Andrew'},
+        {id: 3, name: 'Andrew'},
+        {id: 4, name: 'Andrew'},
+        {id: 5, name: 'Sasha'}]
+
+    let messagesData = [
+        {id: 1, message: 'Yo1'},
+        {id: 2, message: 'Yo2'},
+        {id: 3, message: 'Yo3'},
+        {id: 4, message: 'Yo4'},
+        {id: 5, message: 'Yo5'}]
+
+    return (
+        <div className={s.dialogs_container_flex}>
+            <div className={s.dialogs_items}>
+                <div className={s.name}>Dialogs</div>
+                <ul>
+                    <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                    <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                    <DialogItem name={'Maks3'} id={3}/>
+                </ul>
+            </div>
+            <div className={s.dialogs_messages}>
+                <Message text={messagesData[0].message}/>
+                <FriendMassage text={'Hi I am friend'}/>
+            </div>
+        </div>
+    );
+}
+
+const Message = (props: MessagePropsType) => {
+    return (
         <div className={s.user_message}>
             {props.text}
         </div>
     );
 }
-const FriendMassage = (props:MessagePropsType)=>{
-    return(
+
+const FriendMassage = (props: MessagePropsType) => {
+    return (
         <div className={s.friend_message}>
             {props.text}
         </div>
     );
 }
 
-export function Dialogs() {
+const DialogItem = (props: DialogsItemPropsType) => {
     return (
-        <div className={s.dialogs_container_flex}>
-            <div className={s.dialogs_items}>
-                <div className={s.name}>Dialogs</div>
-                <ul>
-                    <DialogItem name={'Maks1'} id={1}/>
-                    <DialogItem name={'Maks2'} id={2}/>
-                    <DialogItem name={'Maks3'} id={3}/>
-                </ul>
-            </div>
-            <div className={s.dialogs_messages}>
-                <Message text={'Hello'}/>
-                <FriendMassage text={'Hi I am friend'} />
-            </div>
-        </div>
+        <li><NavLink to={`/dialogs/${props.id}`} className={s.item} activeClassName={s.itemActive}>{props.name}</NavLink></li>
     );
 }
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
+    let postsData = [
+        {id: 1, message: 'Hi how are you', likesCount: 10},
+        {id: 2, message: 'It\'s my first post', likesCount: 20},
+    ]
     return (
         <>
             <div>
@@ -12,8 +16,8 @@ export const MyPosts = () => {
                 </div>
             </div>
             <div>
-                <Post message={'HI'} likesCount={20}/>
-                <Post message={'Hello'} likesCount={10}/>
+                <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
+                <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
             </div>
         </>
     );
